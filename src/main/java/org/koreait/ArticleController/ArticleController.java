ac07fe7c41ad2500c7d63639ab2fa68ff55821d8
Pive0528 <pive0528@gmail.com>
2024-07-15 13:49:17 +0900

    기능추가

--- src/main/java/org/koreait/ArticleController/ArticleController.java
+++
@@ -1,4 +0,0 @@
-package org.koreait.ArticleController;
-
-public class ArticleController {
-}
--- src/main/java/org/koreait/Controller.java
+++
@@ -1,7 +0,0 @@
-package org.koreait;
-
-public class Controller {
-    public static void main(String[] args) {
-
-    }
-}
